0e98de09ccdcb2bfeb535d49728e1341ba416295
Jeffrey Davis <j.davis081248@gmail.com>
2023-11-20 02:16:38 -0600

    Completed up to 4

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your code here...
+fetch("http://localhost:3000/menu")
+.then(response => response.json())
+.then(foods => addFoodToMenu(foods))
+
+function addFoodToMenu(foods)
+{
+    foods.forEach(food => {
+        const newSpanElement = document.createElement("span")
+        newSpanElement.innerText = food.name
+        const menuDiv = document.getElementById("menu-items")
+        menuDiv.appendChild(newSpanElement)
+
+        newSpanElement.addEventListener("click", () => displayMainFoodItem(food))
+    })
+,
+    displayMainFoodItem(foods[0])
+    
+    // const menuItemsElement = document.getElementById("menu-items")
+    // menuItemsElement.addEventListener("click", console.log("working?"))
+}
+
+function displayMainFoodItem(food)
+{
+    const dishImage = food.image
+    const imageElement = document.getElementById("dish-image")
+    imageElement.src = dishImage
+
+    const dishName = food.name
+    const nameElement = document.getElementById("dish-name")
+    nameElement.textContent = dishName
+
+    const dishDescription = food.description
+    const descriptionElement = document.getElementById("dish-description")
+    descriptionElement.textContent = dishDescription
+
+    const dishPrice = food.price
+    const priceElement = document.getElementById("dish-price")
+    priceElement.textContent = "$" + dishPrice
+
+    // const menuItemsElement = document.getElementById("menu")
+    // menuItemsElement.addEventListener("click", console.log("working"))
+}
